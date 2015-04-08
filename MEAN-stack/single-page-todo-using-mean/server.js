ef9ce4bfcaf7f41ad3298cbd32f11613d28b016d
helcioajr <helcioajr@gmail.com>
2015-04-08 11:47:33 -0300

    Added MEAN
    :)

---
+++ MEAN-stack/single-page-todo-using-mean/server.js
@@ -0,0 +1,30 @@
+//server.js
+
+// set up
+
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// config
+
+mongoose.connect('mongodb://localhost/node-todo');
+
+app.use(express.static(__dirname + '/public')); //set the static files location
+app.use(morgan('dev')); // log requests
+app.use(bodyParser.urlencoded({
+  'extended': 'true'
+})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.json({
+  type: 'application/vnd.api+json'
+})); // parse application/json
+app.use(methodOverride());
+
+// Listen (start server.js)
+
+app.listen(8080);
+console.log("App listening on port 8080");
