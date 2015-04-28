77a495c177c026edff169a2e4219d842c2ff1c64
helcioajr <helcioajr@gmail.com>
2015-04-28 10:44:51 -0300

    login method working

--- MEAN-stack/userStory/app/routes/api.js
+++ MEAN-stack/userStory/app/routes/api.js
@@ -1,6 +1,20 @@
 var User = require('../models/user');
 var config = require('../../config');
+var jsonwebtoken = require('jsonwebtoken');
 var secretKey = config.secretKey;
+
+
+function createToken(user) {
+    var token = jsonwebtoken.sign({
+        _id: user._id,
+        name: user.name,
+        username: user.username
+    }, secretKey, {
+        expiresInMinute: 1440
+    });
+
+    return token;
+}
 
 module.exports = function(app, express) {
     var api = express.Router();
@@ -24,11 +38,10 @@ module.exports = function(app, express) {
         });
     });
 
-
     api.get('/users', function(req, res) {
 
         User.find({}, function(err, users) {
-            if(err) {
+            if (err) {
                 res.send(err);
                 return;
             }
@@ -38,6 +51,42 @@ module.exports = function(app, express) {
         });
 
     });
+
+    api.post('/login', function(req, res) {
+
+        User.findOne({
+            username: req.body.username
+        }).select('password').exec(function(err, user) {
+
+            if (err) throw err;
+
+            if (!user) {
+
+                res.send({
+                    message: "User doesn't exist."
+                });
+
+            } else if (user) {
+
+                var validPassword = user.comparePassword(req.body.password);
+
+                if (!validPassword) {
+                    res.send({
+                        message: "Invalid password."
+                    });
+
+                } else {
+                    var token = createToken(user);
+
+                    res.json({
+                        success: true,
+                        message: "Successfully logged in!",
+                        token: token
+                    });
+                }
+            }
+        })
+    })
 
     return api;
 }
--- MEAN-stack/userStory/config.js
+++ MEAN-stack/userStory/config.js
@@ -2,5 +2,5 @@ module.exports = {
 
     "database": "mongodb://root:abc123@ds061767.mongolab.com:61767/userstory",
     "port": process.env.PORT || 3000,
-    "secreKey": "feartheoldblood"
+    "secretKey": "feartheoldblood"
 }
